7abcf6fd636912ca5b2cb8d3b8a857ce99b997ba
Jessie <jessica.v.rid@gmail.com>
2023-01-27 18:32:08 +0300

    task 3 added

--- Task_2_3_points/Program.cs
+++ Task_2_3_points/Program.cs
@@ -1,4 +1,11 @@
-﻿double [] coordsA = new double [3];
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+double [] coordsA = new double [3];
 double [] coordsB = new double [3];
 
 
@@ -20,4 +27,4 @@ coordsB [2] = Convert.ToInt32(Console.ReadLine());
 
 double S = Math.Sqrt(Math.Pow(coordsA[0]-coordsB[0], 2) + Math.Pow(coordsA[1]-coordsB[1], 2) + Math.Pow(coordsA[2]-coordsB[2], 2));
 
-System.Console.WriteLine(S);
+System.Console.WriteLine($"расстояние между ними в 3D пространстве= {S}");
