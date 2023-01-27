9d0b35e7ac9e2366b14ca8ca64daadf96a5ef0c5
Jessie <jessica.v.rid@gmail.com>
2023-01-27 17:56:53 +0300

    task_2 added

---
+++ Task_2_3_points/Program.cs
@@ -0,0 +1,23 @@
+﻿double [] coordsA = new double [3];
+double [] coordsB = new double [3];
+
+
+System.Console.WriteLine("Введите координату Х точки А: ");
+coordsA [0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки А: ");
+coordsA [1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Z точки А: ");
+coordsA [2] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координату Х точки B: ");
+coordsB [0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки B: ");
+coordsB [1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Z точки B: ");
+coordsB [2] = Convert.ToInt32(Console.ReadLine());
+
+
+
+double S = Math.Sqrt(Math.Pow(coordsA[0]-coordsB[0], 2) + Math.Pow(coordsA[1]-coordsB[1], 2) + Math.Pow(coordsA[2]-coordsB[2], 2));
+
+System.Console.WriteLine(S);
